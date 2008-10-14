acff124d06a34617a7fb1c4d0cb88551fb5a416a
Stas Fomin <stas-fomin@yandex.ru>
2008-10-14 15:02:05 +0000

    Bug 25536

---
+++ DocExport.i18n.php
@@ -0,0 +1,27 @@
+<?php
+#coding: utf-8
+/** \file
+* \brief Internationalization file for the User Merge and Delete Extension.
+*/
+
+
+$messages = array();
+
+$messages['en'] = array(
+    'docexport-msword-export-link'          => '→M$WORD',
+    'docexport-oo-export-link'              => '→OOffice',
+    'docexport-purge-tab'                   => 'purge',
+    'tooltip-ca-export2word'                => 'Export to MS Word',
+    'tooltip-ca-export2oo'                  => 'Export to Open Office',
+    'tooltip-ca-purge'                      => 'Purge/refresh article, clear cache…',
+);
+
+$messages['ru'] = array(
+    'docexport-msword-export-link'          => '→M$WORD',
+    'docexport-oo-export-link'              => '→OOffice',
+    'docexport-purge-tab'                   => 'Обновить',
+    'tooltip-ca-export2word'                => 'Экспорт в MS Word',
+    'tooltip-ca-export2oo'                  => 'Экспорт в Open Office',
+    'tooltip-ca-purge'                      => 'Обновить статью, сбросить кеш…',
+);
+
